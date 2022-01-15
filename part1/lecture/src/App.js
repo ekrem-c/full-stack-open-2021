c988b223cd8ef513f64107b691dd11ed08023fc8
Ekrem Cologlu <ekremcologlu@gmail.com>
2022-01-15 14:40:13 -0600

    end of passing state to child components

--- part1/lecture/src/App.js
+++ part1/lecture/src/App.js
@@ -21,20 +21,44 @@ const Footer = () => {
   )
 }
 
-const App = (props) => {
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+
+const App = () => {
   const [counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
-  console.log("rendering...", counter)
 
   return (
-    
     <div>
-      {counter}
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text="plus"
+      />
+      <Button
+        onClick={decreaseByOne}
+        text="minus"
+      />
+      <Button
+        onClick={setToZero}
+        text="zero"
+      />
     </div>
   )
 }
