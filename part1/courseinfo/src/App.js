115df57bd8764c60c631a9d4ff3bde619230bfa4
ekrem-c <ekremcologlu@gmail.com>
2023-06-28 19:26:06 -0500

    exercise 2.4: extend the application to allow arbitrary number of courses

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Course from './components/Course'
+import {Courses} from './components/Course'
 
 //I left at unit 1.c
 
@@ -41,35 +41,79 @@ export const Total = ({course}) => {
 //{course["parts"][0].exercises + course["parts"][1].exercises + course["parts"][2].exercises}</p></b>
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: "Echo test",
-        exercises: 110,
-        id: 4
-      }
-    ]
-    
-  }
+  // const course = {
+  //   name: "Half Stack application development",
+  //   parts: [
+  //     {
+  //       name: "Fundamentals of React",
+  //       exercises: 10,
+  //       id: 1
+  //     },
+  //     {
+  //       name: "Using props to pass data",
+  //       exercises: 7,
+  //       id: 2
+  //     },
+  //     {
+  //       name: "State of a component",
+  //       exercises: 14,
+  //       id: 3
+  //     },
+  //     {
+  //       name: "Echo test",
+  //       exercises: 110,
+  //       id: 4
+  //     }
+  //   ]
+  // }
+
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
   return (
     <div>
-      <Course course={course}/>
+      <Courses courses={courses}/>
     </div>
   )
 }
--- part1/courseinfo/src/components/Course.js
+++ part1/courseinfo/src/components/Course.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import {Header, Content, Total} from '../App'
 
+export const Courses = (props) => {
+  const courses = props.courses;
+  return (
+    <>
+    {courses.map(course =>
+      <Course key={course.id} course={course}/>
+      )}
+    </>
+  )
+}
+
 const Course = (props) => {
-  console.log(props);
+  //console.log(props);
   const {course} = props;
   return (
     <div>
