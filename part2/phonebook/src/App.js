5b65a19a9c74cfa443804c3c0aa0db63c3a77492
ekrem-c <ekremcologlu@gmail.com>
2023-07-04 15:45:44 -0500

    finished exercise 2.9 filter function. Exercise 10 will be skipped

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,26 +4,42 @@ import Content from './components/Content'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
+
   ]);
   const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   }
 
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value);
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  }
+
   const addPerson = (event) => {
     event.preventDefault();
     const personObject = {
-      name: newName
+      name: newName,
+      phone: newPhone
     }
 
+    setNewName('');
+    setNewPhone('');
     if (persons.filter(person => person.name === personObject.name).length > 0) {
       alert(`${personObject.name} is already added to phonebook`);
       return;
     }
     setPersons(persons.concat(personObject));
-    setNewName('');
   }
   
 
@@ -35,11 +51,18 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          phone: <input value={newPhone} onChange={handlePhoneChange}/>
+        </div>
+        <div>
           <button onClick={addPerson} type="submit">add</button>
         </div>
       </form>
+      <h3>Filter</h3>
+      <div>
+          names starting with: <input value={filter} onChange={handleFilterChange}/>
+        </div>
       <h2>Numbers</h2>
-      <Content persons={persons}/>
+      <Content persons={persons} filter={filter}/>
     </div>
   );
 }
--- part2/phonebook/src/components/Content.js
+++ part2/phonebook/src/components/Content.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 const Person = ({ person }) => {
   return (
-    <li>{person.name}</li>
+    <li>{person.name} --- {person.phone}</li>
   )
 }
 
 const Content = (props) => {
   const numbers = props.persons;
+  const filter = props.filter;
   return (
     <>
-      {numbers.map(number =>
+      {numbers.filter(number => number.name.toLowerCase().startsWith(filter.toLowerCase())).map(number =>
         <Person key={number.name} person={number}/>)}
     </>
   );
