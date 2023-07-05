7f3ce8461fe1995a416e8c15ca6821c5dfda75b1
ekrem-c <ekremcologlu@gmail.com>
2023-07-04 20:48:11 -0500

    finished exercise 2.11 axios

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,25 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Content from './components/Content'
+import axios from 'axios'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
   const [filter, setFilter] = useState('');
+
+  const hook = () => {
+    console.log('effect');
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled');
+      setPersons(response.data);
+    })
+  };
+
+  useEffect(hook, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
--- part2/phonebook/src/components/Content.js
+++ part2/phonebook/src/components/Content.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Person = ({ person }) => {
   return (
-    <li>{person.name} --- {person.phone}</li>
+    <li>{person.name} --- {person.number}</li>
   )
 }
 
@@ -12,7 +12,7 @@ const Content = (props) => {
   return (
     <>
       {numbers.filter(number => number.name.toLowerCase().startsWith(filter.toLowerCase())).map(number =>
-        <Person key={number.name} person={number}/>)}
+        <Person key={number.id} person={number}/>)}
     </>
   );
 }
